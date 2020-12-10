caa3464015e719f0f3eb6ac345c75a5744cf46da
Daniel Castro <kr4usdc@gmail.com>
2020-12-09 22:45:57 -0300

    wednesday
    	modified:   index.html
    	modified:   public/css/animations.css
    	modified:   public/css/main.css
    	modified:   public/images/icon-facebook.svg
    	modified:   public/images/icon-instagram.svg
    	modified:   public/images/icon-pinterest.svg
    	modified:   public/images/icon-twitter.svg
    	modified:   public/scripts/main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,7 @@
+// Burguer Menu Opener
 const burgerBtn = document.querySelector('.menu-btn');
+const navPurple = document.querySelector('.nav-purple');
 burgerBtn.addEventListener('click',() => {
     burgerBtn.classList.toggle('burger-open');
+    navPurple.classList.toggle('hidden');
 });
