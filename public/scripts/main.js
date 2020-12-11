3cfdbb94184e017cbbae4aaec0498131673cfc9c
Daniel Castro <kr4usdc@gmail.com>
2020-12-11 19:21:24 -0300

    dec 11

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,3 +5,14 @@ burgerBtn.addEventListener('click',() => {
     burgerBtn.classList.toggle('burger-open');
     navPurple.classList.toggle('hidden');
 });
+
+//document.querySelector('form input').oninvalid = function() { // remove mensagens de erro padrão this.setCustomValidity(""); // faz a validação novamente if (!this.validity.valid) { // se estiver inválido, coloca a mensagem this.setCustomValidity("Nome está inválido!"); } };
+
+document.querySelector('.shortener input').oninvalid = function() { 
+    this.setCustomValidity("");  
+    if (!this.validity.valid) {  
+        this.setCustomValidity(' '); 
+        this.setAttribute("style", "border: 4px solid red;")
+        this.classList.toggle('placeholder-invalid');
+    } 
+};
