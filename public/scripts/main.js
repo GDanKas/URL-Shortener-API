1f00ed1dc98258f2c693bb540f24da40d4f954de
Daniel Castro <kr4usdc@gmail.com>
2021-04-10 17:58:09 -0300

    apr 10 2021

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,18 @@
 //document.querySelector('form input').oninvalid = function() { // remove mensagens de erro padrão this.setCustomValidity(""); // faz a validação novamente if (!this.validity.valid) { // se estiver inválido, coloca a mensagem this.setCustomValidity("Nome está inválido!"); } };
 const shortener = document.querySelector('.shortener');
 const shortenerInput = document.querySelector('.shortener input');
-const shortenerButton = document.querySelector('.shortener .btn-squared');
+const shortenerButton = document.getElementById('main-button');
 const shortenedLinks = document.querySelector('.shortened-links');
 const linkContainer = document.querySelectorAll('.shortened-links .link-container');
 const shortenedButtons = document.querySelectorAll('.shortened-links .link-container .btn-squared');
 
-console.log(shortenedButtons);
-
 let signCreated = false;
-shortenerInput.oninvalid = setInputError;
+//shortenerInput.oninvalid = setInputError;
 shortenedButtons.forEach(element => {
     element.addEventListener("click", copyLink);
 });
+
+shortenerButton.addEventListener("click", () => fetchAPI)
 
 function setInputError() {
     this.setCustomValidity("");
@@ -28,31 +28,43 @@ function setInputError() {
             shortener.insertBefore(inputLegend, btn);
             signCreated = true;
         }
-
     }
 }
 
-/*function copyLink() {
-    let arr = this.parentNode.parentNode.childNodes
-    console.log(arr)
-    console.log(this.parentNode.parentNode.childNodes[5].childNodes[7])
-    if (this.parentNode.childNodes[7].classList[0] === 'btn-squared') {
-        this.parentNode.childNodes[7].classList.toggle('btn-squared-active');
-    }
-}*/
-
 function copyLink() {
-    console.log(this.parentNode.parentNode.childNodes)
     if (this.classList[0] == 'btn-squared') {
         for (let i=1; i < this.parentNode.parentNode.childNodes.length; i+= 2) {
-            this.parentNode.parentNode.childNodes[i].childNodes[7].classList.remove('btn-squared-active'); 
-            this.innerText = "Copy!";
+            this.parentNode.parentNode.childNodes[i].childNodes
+            [this.parentNode.parentNode.childNodes.length].classList.remove('btn-squared-active'); 
+            this.parentNode.parentNode.childNodes[i].childNodes
+            [this.parentNode.parentNode.childNodes.length].innerText = "Copy!";
         }
         this.classList.toggle('btn-squared-active');
-        if (this.innerText === 'Copy!')
+        if (this.innerText == "Copy!")
         this.innerText = "Copied!";
         else 
         this.innerText = "Copy!";
     } 
-    console.log(this.parentNode.parentNode.childNodes);
+    var textToCopy = this.parentNode.childNodes[5].innerText;
+
+    var myTemporaryInputElement = document.createElement('input');
+    myTemporaryInputElement.type = 'text';
+    myTemporaryInputElement.value = textToCopy;
+ 
+    document.body.appendChild(myTemporaryInputElement);
+ 
+    myTemporaryInputElement.select();
+    document.execCommand('Copy');
+ 
+    document.body.removeChild(myTemporaryInputElement);
 } 
+
+console.log(shortenerInput.value)
+
+function fetchAPI() {
+    let x = shortenerInput.value;
+    console.log(x)
+    fetch(`https://api.shrtco.de/v2/shorten?url=${x}`)
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
