61274da43cdf7a0562d0904f42df4f594d20f37e
GDanKas <kr4usdc@gmail.com>
2021-04-12 18:13:09 -0300

    apr 12 2021

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,20 +1,60 @@
 //document.querySelector('form input').oninvalid = function() { // remove mensagens de erro padrão this.setCustomValidity(""); // faz a validação novamente if (!this.validity.valid) { // se estiver inválido, coloca a mensagem this.setCustomValidity("Nome está inválido!"); } };
-const shortener = document.querySelector('.shortener');
-const shortenerInput = document.querySelector('.shortener input');
-const shortenerButton = document.getElementById('main-button');
-const shortenedLinks = document.querySelector('.shortened-links');
-const linkContainer = document.querySelectorAll('.shortened-links .link-container');
-const shortenedButtons = document.querySelectorAll('.shortened-links .link-container .btn-squared');
+let shortener = document.querySelector('.shortener');
+let shortenerInput = document.querySelector('.shortener input');
+let shortenerButton = document.getElementById('main-button');
+let shortenedLinks = document.querySelector('.shortened-links');
+let linkContainer = document.querySelectorAll('.shortened-links .link-container');
+let shortenedButtons = document.querySelectorAll('.shortened-links .link-container .btn-squared');
 
 let signCreated = false;
 //shortenerInput.oninvalid = setInputError;
-shortenedButtons.forEach(element => {
-    element.addEventListener("click", copyLink);
-});
+function shortenedRefresher() {
+    let shortenedButtons = document.querySelectorAll('.shortened-links .link-container .btn-squared');
+                shortenedButtons.forEach(element => {
+                    element.addEventListener("click", copyLink);
+                });
+}
+
+shortenedRefresher();
 
 shortenerButton.addEventListener("click", () => fetchAPI)
 
-function setInputError() {
+function setInputError2() {
+    let isvalidURL = validURL(shortenerInput.value);
+    if (!isvalidURL) {
+        shortenerInput.setAttribute("style", "border: 2px solid var(--error); ")
+        shortenerInput.classList.add('placeholder-invalid');
+        if (!signCreated) {
+            let inputLegend = document.createElement("p");
+            inputLegend.innerText = "Please add a link";
+            let btn = document.querySelector(".shortener .btn-squared")
+            shortener.insertBefore(inputLegend, btn);
+            signCreated = true;
+        }
+        return false;
+    } else {
+        shortenerInput.removeAttribute("style");
+        shortenerInput.classList.remove('placeholder-invalid');
+        if (signCreated) {
+            let sign = document.querySelector('.shortener p');
+            shortener.removeChild(sign);
+            signCreated = false;
+        }
+        return true;
+    }
+}
+
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
+}
+
+/*function setInputError() {
     this.setCustomValidity("");
     shortenerInput.value = "";
     if (!this.validity.valid) {
@@ -28,43 +68,74 @@ function setInputError() {
             shortener.insertBefore(inputLegend, btn);
             signCreated = true;
         }
-    }
-}
+    } 
+}*/
 
 function copyLink() {
     if (this.classList[0] == 'btn-squared') {
-        for (let i=1; i < this.parentNode.parentNode.childNodes.length; i+= 2) {
+        /*for (let i = 1; i < this.parentNode.parentNode.childNodes.length; i += 2) {
             this.parentNode.parentNode.childNodes[i].childNodes
             [this.parentNode.parentNode.childNodes.length].classList.remove('btn-squared-active'); 
+            console.log(this.parentNode.parentNode.childNodes[i].childNodes[shortenedLinks.length])
             this.parentNode.parentNode.childNodes[i].childNodes
             [this.parentNode.parentNode.childNodes.length].innerText = "Copy!";
-        }
+        }*/
+        linkContainer.forEach(element => {
+            let buttons = element.querySelectorAll('.btn-squared');
+            buttons.classList.remove('btn-squared-active');
+            buttons.innerText = "Copy!";
+            console.log(buttons)
+        });
         this.classList.toggle('btn-squared-active');
         if (this.innerText == "Copy!")
-        this.innerText = "Copied!";
-        else 
-        this.innerText = "Copy!";
-    } 
-    var textToCopy = this.parentNode.childNodes[5].innerText;
+            this.innerText = "Copied!";
+        else
+            this.innerText = "Copy!";
+    }
+    let textToCopy = this.parentNode.childNodes[2].innerText;
 
-    var myTemporaryInputElement = document.createElement('input');
+    let myTemporaryInputElement = document.createElement('input');
     myTemporaryInputElement.type = 'text';
     myTemporaryInputElement.value = textToCopy;
- 
+
     document.body.appendChild(myTemporaryInputElement);
- 
+
     myTemporaryInputElement.select();
     document.execCommand('Copy');
- 
-    document.body.removeChild(myTemporaryInputElement);
-} 
 
-console.log(shortenerInput.value)
+    document.body.removeChild(myTemporaryInputElement);
+}
 
 function fetchAPI() {
     let x = shortenerInput.value;
     console.log(x)
-    fetch(`https://api.shrtco.de/v2/shorten?url=${x}`)
-    .then(res => res.json())
-    .then(data => console.log(data));
+    if (setInputError2()) {
+        fetch(`https://api.shrtco.de/v2/shorten?url=${x}`)
+            .then(res => res.json())
+            .then(data => {
+                let newLinkContainer = document.createElement('div');
+                newLinkContainer.classList.add('link-container');
+                let oldLinkSpan = document.createElement('span');
+                oldLinkSpan.classList.add('old-link');
+                oldLinkSpan.innerText = data.result.original_link;
+                let newLinkHR = document.createElement('hr');
+                let newLinkSpan = document.createElement('span');
+                newLinkSpan.classList.add('new-link');
+                newLinkSpan.innerText = data.result.short_link;
+                let copyButton = document.createElement('button');
+                copyButton.classList.add('btn-squared');
+                let buttonSpan = document.createElement('span');
+                buttonSpan.innerText = 'Copy!';
+
+                copyButton.appendChild(buttonSpan);
+                newLinkContainer.appendChild(oldLinkSpan);
+                newLinkContainer.appendChild(newLinkHR);
+                newLinkContainer.appendChild(newLinkSpan);
+                newLinkContainer.appendChild(copyButton);
+                shortenedLinks.appendChild(newLinkContainer);                
+
+                console.log(data.result);
+                shortenedRefresher();
+            });
+    }
 }
